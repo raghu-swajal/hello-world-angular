f3935aa2983f78da70fc5215d2266ddd58fda16a
Raghu Pratap Singh <raghu.s@helloboon.com>
2022-10-18 14:43:38 +0530

    data binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,11 @@ import { Component } from "@angular/core";
 })
 
 export class ServerComponent{
+    serverId :number = 10;
+    serverStatus : string = 'offline';
 
+    //Returning server status using method:
+    getServerStatus(){
+        return this.serverStatus;
+    }
 }
